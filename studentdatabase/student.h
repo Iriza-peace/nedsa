690e2a837ca06a1edf250adb53896079bfbadca9
Iriza-peace <irizapeace@gmail.com>
2024-06-20 09:02:31 +0300

    added employment management system

---
+++ studentdatabase/student.h
@@ -0,0 +1,16 @@
+//
+// Created by User on 20/06/2024.
+//
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+
+class student {
+
+};
+
+
+
+#endif //STUDENT_H
---
+++ studentregistration/students.h
@@ -0,0 +1,16 @@
+//
+// Created by User on 20/06/2024.
+//
+
+#ifndef STUDENTS_H
+#define STUDENTS_H
+
+
+
+class students {
+
+};
+
+
+
+#endif //STUDENTS_H
